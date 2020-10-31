973e8e88e201cfbd0e984990fa0eb66dd0570e88
Marco <markorovi24@gmail.com>
2020-10-31 14:02:27 -0600

    Test

---
+++ src/com/gmail/markorovi24/GUI/test.java
@@ -0,0 +1,10 @@
+package com.gmail.markorovi24.GUI;
+
+
+public class test {
+    public static void main(String[] args){
+        Ventana test = new Ventana();
+        test.crearVentana("hola", 100,100);
+        test.habilitarVentana();
+    }
+}
