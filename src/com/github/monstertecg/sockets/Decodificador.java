880927ff330f235d6a2fe0c65fb7f4dcf3690daf
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2020-11-02 18:03:10 -0600

    añadiendo Decodificador

---
+++ src/com/github/monstertecg/sockets/Decodificador.java
@@ -0,0 +1,12 @@
+package com.github.monstertecg.sockets;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class Decodificador {
+
+    private static JsonNode jsonNode;
+
+    private static JsonNode Decodificar (String mensajeCodificado) {
+        return null;
+    }
+}
