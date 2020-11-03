8277ad15c327dc9fe8db44b0a6f22a20eb58adb4
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2020-11-02 18:47:50 -0600

    añadiendo Decodificador 2

--- src/com/github/monstertecg/sockets/Decodificador.java
+++ src/com/github/monstertecg/sockets/Decodificador.java
@@ -1,12 +1,29 @@
 package com.github.monstertecg.sockets;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.monstertecg.json.Json;
+
+import java.io.IOException;
 
 public class Decodificador {
 
-    private static JsonNode jsonNode;
+    private static void Decodificar (String mensajeCodificado) throws IOException {
+        JsonNode jsonNode = Json.parse(mensajeCodificado);
+        String tipoCarta = jsonNode.get("Carta seleccionada").asText();
+        String idCarta = jsonNode.get("id").asText();
 
-    private static JsonNode Decodificar (String mensajeCodificado) {
-        return null;
+        // Asegura el tipo de carta
+
+        switch (tipoCarta){
+            case "esbirros":
+                switch (idCarta) {
+                    case "0":
+                        // llamar clase de esbirros
+                }
+                break;
+        }
+        /*
+            Llama al medidador de cartas
+         */
     }
 }
