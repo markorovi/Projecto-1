7e74a8ea4013fd2a9db710e700cb02483ab0f939
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2020-11-03 18:48:31 -0600

    mejora en Cartas.java

--- src/com/gmail/markorovi24/Cartas/Hechizos.java
+++ src/com/gmail/markorovi24/Cartas/Hechizos.java
@@ -2,8 +2,9 @@ package com.gmail.markorovi24.Cartas;
 
 public class Hechizos extends Cartas {
 
-    public Hechizos(String nombre, String descripcion, int mana) {
-        super(nombre, descripcion, null, 0, mana);
+
+    public Hechizos(String id, String nombre, String descripcion, int mana) {
+        super(id, nombre, descripcion, mana);
     }
 
     @Override
